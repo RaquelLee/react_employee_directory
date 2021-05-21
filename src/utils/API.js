b861e148c68a86ddac2564e815c29d56f67b8992
rsco <RaquelLee@users.noreply.github.com>
2021-05-21 11:18:52 -0400

    random user API used

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,13 +1,9 @@
 import axios from "axios";
 
-// Export an object containing methods we'll use for accessing the random user API
-//https://randomuser.me/documentation
-// save load time by including fields
-// https://randomuser.me/api/?format=pretty&results=10&nat=us&inc=name,email,picture
-// results, then iterate through
-
-export default {
+const exportDefault = {
     getEmployees: function () {
         return axios.get("https://randomuser.me/api/?format=pretty&results=10&nat=us&inc=name,email,picture");
     }
 };
+
+export default exportDefault;
